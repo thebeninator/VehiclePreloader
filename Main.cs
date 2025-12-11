51bec6dd447c54b595a7c058e95f5d4d6e96aacf
Benjamin Nguyen <xccr123@gmail.com>
2025-12-10 18:26:10 -0800

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,29 @@
+﻿using GHPC.Mission;
+using MelonLoader;
+using UnityEngine;
+using VehiclePreloader;
+
+[assembly: MelonInfo(typeof(Mod), "Vehicle Preloader", "1.0.0", "ATLAS")]
+[assembly: MelonGame("Radian Simulations LLC", "GHPC")]
+
+namespace VehiclePreloader
+{
+    public class Mod : MelonMod
+    {
+        internal static bool done = false;
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (done || (sceneName != "MainMenu2_Scene" && sceneName != "MainMenu2-1_Scene" && sceneName != "t64_menu")) return;
+
+            UnitPrefabLookupScriptable lookup = Resources.FindObjectsOfTypeAll<UnitPrefabLookupScriptable>()[0];
+            UnitPrefabLookupScriptable.UnitPrefabMetadata[] all_units = lookup.AllUnits;
+
+            foreach (var unit in all_units)
+            {
+                unit.PrefabReference.LoadAssetAsync<GameObject>().WaitForCompletion();
+            }
+
+            done = true;
+        }
+    }
+}
